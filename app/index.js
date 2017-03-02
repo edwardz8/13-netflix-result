31b9808c6db6cc72b12528bad28c7eeb4f9b8271
Zach Edwards <tolklorist@gmail.com>
2017-03-02 15:34:19 -0600

    changes

--- app/index.js
+++ app/index.js
@@ -1,5 +1,21 @@
 import 'whatwg-fetch';
+import fillInResult from './fill-in-result';
 
-const searchForm = element.innerHTML('.');
-const searchInput = element.innerHTML('.');
-const searchResult = element.innerHTML('.');
+const searchForm = document.querySelector('.search-button');
+const searchInput = document.querySelector('.search__bar');
+const searchResult = document.querySelector('.movie-profile');
+
+function startSearch(term) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${term}`)
+  .then((res) => res.json())
+  .then((data) => {
+    fillInResult(searchResult, data);
+  });
+}
+
+searchForm.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  startSearch(searchInput.value);
+});
+startSearch('Attack on Titan');
